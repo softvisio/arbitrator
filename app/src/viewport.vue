994c5d8409b00aca0fd7e5e0b96a2c70763e09d7
zdm <zdm@softvisio.net>
2021-09-30 13:28:28 +0300

    feat: webpack migration

--- app/src/viewport.vue
+++ app/src/viewport.vue
@@ -6,8 +6,8 @@ export default {
     "extends": Viewport,
 
     created () {
-        this.publicView = defineAsyncComponent( () => import( "@/views/public.vue" ) );
-        this.privateView = defineAsyncComponent( () => import( "@/views/private.vue" ) );
+        this.publicView = defineAsyncComponent( () => import( /* webpackChunkName: "public" */ "@/views/public.vue" ) );
+        this.privateView = defineAsyncComponent( () => import( /* webpackChunkName: "private" */ "@/views/private.vue" ) );
     },
 
     "methods": {
