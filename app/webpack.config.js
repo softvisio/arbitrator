94047b05b0309b88c0f8b9aa0abd5bce2bde2469
zdm <zdm@softvisio.net>
2022-02-11 18:50:00 +0200

    feat: app env schema validator

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,5 +1,5 @@
 import config from "#vue/webpack.config.vue-app";
 
-config.schemas.push( new URL( "./resources/schemas/vue-app.env.schema.yaml", import.meta.url ) );
+config.schemas.push( new URL( "./resources/schemas/env.vue-app.schema.yaml", import.meta.url ) );
 
 export default [config];
