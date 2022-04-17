2e7e60b5f4cad64abf90a37f0fc194e90d5d6e46
zdm <zdm@softvisio.net>
2022-04-17 18:58:49 +0300

    feat: amcharts5

--- app/src/views/private/team/panel.vue
+++ app/src/views/private/team/panel.vue
@@ -20,6 +20,8 @@
                 <ext-button text="Refresh" @tap="_reloadCreatives"/>
             </ext-toolbar>
 
+            <AmchartsPanel height="300" :animated="false" :responsive="true" :micro="false" @ready="_amchartReady"/>
+
             <ext-chart flex="1" title="chart 1" @ready="chartReady"/>
 
             <ext-grid scrollToTopOnRefresh1="true" @ready="_gridReady">
@@ -49,6 +51,9 @@ import { defineAsyncComponent } from "vue";
 import "#vue/components/froala-editor";
 import "#vue/components/ext-charts";
 
+import AmchartsPanel from "#vue/components/amcharts5/panel";
+import * as am5xy from "@amcharts/amcharts5/xy";
+
 const StreamsPanel = defineAsyncComponent( () => import( "./streams/panel" ) );
 
 // const CreativesPanel = defineAsyncComponent( () => import( "./creatives/panel" ) );
@@ -58,7 +63,7 @@ const Dialog = defineAsyncComponent( () => import( /* webpackPrefetch: true */ "
 // import Dialog from "./dialog";
 
 export default {
-    "components": { StreamsPanel },
+    "components": { StreamsPanel, AmchartsPanel },
 
     "methods": {
         _gridReady ( e ) {
@@ -163,6 +168,131 @@ export default {
                     { "name": "Item-14", "g1": 16.87, "g2": 56.8 },
                 ],
             } );
+        },
+
+        // XXX
+        _amchartReady ( cmp ) {
+            const root = cmp.root,
+                am5 = cmp.am5;
+
+            var chart = root.container.children.push( am5xy.XYChart.new( root, {
+                "panX": true,
+                "panY": true,
+                "wheelX": "panX",
+                "wheelY": "zoomX",
+                "pinchZoomX": true,
+            } ) );
+
+            // Add cursor
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+            var cursor = chart.set( "cursor", am5xy.XYCursor.new( root, {} ) );
+            cursor.lineY.set( "visible", false );
+
+            // Create axes
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+            var xRenderer = am5xy.AxisRendererX.new( root, { "minGridDistance": 30 } );
+            xRenderer.labels.template.setAll( {
+                "rotation": -90,
+
+                // "centerY": am5.p50,
+                // "centerX": am5.p100,
+                "paddingRight": 15,
+            } );
+
+            var xAxis = chart.xAxes.push( am5xy.CategoryAxis.new( root, {
+                "maxDeviation": 0.3,
+                "categoryField": "country",
+                "renderer": xRenderer,
+                "tooltip": am5.Tooltip.new( root, {} ),
+            } ) );
+
+            var yAxis = chart.yAxes.push( am5xy.ValueAxis.new( root, {
+                "maxDeviation": 0.3,
+                "renderer": am5xy.AxisRendererY.new( root, {} ),
+            } ) );
+
+            // Create series
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+            var series = chart.series.push( am5xy.ColumnSeries.new( root, {
+                "name": "Series 1",
+                "xAxis": xAxis,
+                "yAxis": yAxis,
+                "valueYField": "value",
+                "sequencedInterpolation": true,
+                "categoryXField": "country",
+                "tooltip": am5.Tooltip.new( root, {
+                    "labelText": "{valueY}",
+                } ),
+            } ) );
+
+            series.columns.template.setAll( { "cornerRadiusTL": 5, "cornerRadiusTR": 5 } );
+            series.columns.template.adapters.add( "fill", function ( fill, target ) {
+                return chart.get( "colors" ).getIndex( series.columns.indexOf( target ) );
+            } );
+
+            series.columns.template.adapters.add( "stroke", function ( stroke, target ) {
+                return chart.get( "colors" ).getIndex( series.columns.indexOf( target ) );
+            } );
+
+            // Set data
+            var data = [
+                {
+                    "country": "USA",
+                    "value": 2025,
+                },
+                {
+                    "country": "China",
+                    "value": 1882,
+                },
+                {
+                    "country": "Japan",
+                    "value": 1809,
+                },
+                {
+                    "country": "Germany",
+                    "value": 1322,
+                },
+                {
+                    "country": "UK",
+                    "value": 1122,
+                },
+                {
+                    "country": "France",
+                    "value": 1114,
+                },
+                {
+                    "country": "India",
+                    "value": 984,
+                },
+                {
+                    "country": "Spain",
+                    "value": 711,
+                },
+                {
+                    "country": "Netherlands",
+                    "value": 665,
+                },
+                {
+                    "country": "Russia",
+                    "value": 580,
+                },
+                {
+                    "country": "South Korea",
+                    "value": 443,
+                },
+                {
+                    "country": "Canada",
+                    "value": 441,
+                },
+            ];
+
+            xAxis.data.setAll( data );
+            series.data.setAll( data );
+
+            // Make stuff animate on load
+            // https://www.amcharts.com/docs/v5/concepts/animations/
+            series.appear( 1000 );
+            chart.appear( 1000, 100 );
         },
     },
 };
